1709773bcdcb892f38255850151301d1c9f99211
pooja khutwad <poojak.dev22@gmail.com>
2023-07-06 12:58:02 +0530

    first commit

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,7 +1,9 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createBuiltinActions, createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import { checkRepositoryExistsAction } from './scaffolder/actions/custom';
+import { ScmIntegrations } from '@backstage/integration';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -10,7 +12,20 @@ export default async function createPlugin(
     discoveryApi: env.discovery,
   });
 
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const builtInActions = createBuiltinActions({
+    integrations,
+    catalogClient,
+    config: env.config,
+    reader: env.reader,
+  });
+
+  const actions = [...builtInActions, checkRepositoryExistsAction()];
+
+
   return await createRouter({
+    actions,
     logger: env.logger,
     config: env.config,
     database: env.database,
